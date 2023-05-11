1449a0530f636ba18f02178b84f83093fa6ca0e7
Ahmad Internaldo <ainternaldo@gmail.com>
2023-05-11 22:07:33 +0700

    first initial

---
+++ utils/bcrypt.ts
@@ -0,0 +1,15 @@
+import * as bcrypt from 'bcrypt';
+
+export const hash = async (hashPassword: string): Promise<string> => {
+  const saltOrRounds: number = 10;
+  const password = await bcrypt.hash(hashPassword, saltOrRounds);
+  return password;
+};
+
+export const matching = async (
+  password: string,
+  hashPassword: string,
+): Promise<boolean> => {
+  const isMatch = await bcrypt.compare(password, hashPassword);
+  return isMatch;
+};
