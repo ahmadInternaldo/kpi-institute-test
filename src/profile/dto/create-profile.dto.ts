9e2ce2d6868e0ad4f195b6e12e9cd02029d0d584
Ahmad Internaldo <ainternaldo@gmail.com>
2023-05-12 03:09:59 +0700

    adding validation field

--- src/profile/dto/create-profile.dto.ts
+++ src/profile/dto/create-profile.dto.ts
@@ -1,5 +1,9 @@
+import { IsNotEmpty } from 'class-validator';
 import { ProfileInterface } from '../interfaces/profile.interface';
 
 export class CreateProfileDto implements ProfileInterface {
+  @IsNotEmpty({
+    message: `profile's name cannot be empty`,
+  })
   profileName: string;
 }
