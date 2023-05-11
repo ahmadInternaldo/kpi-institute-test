b18a2f5eea9c3d46f7b1f8af599a474bd924d249
Ahmad Internaldo <ainternaldo@gmail.com>
2023-05-12 02:59:44 +0700

    setting logic activity

--- src/user/dto/create-user.dto.ts
+++ src/user/dto/create-user.dto.ts
@@ -1,8 +1,11 @@
 import { UserInterface } from '../interfaces/user.interface';
-
+import { IsEmail } from 'class-validator';
 export class CreateUserDto implements UserInterface {
   name: string;
+
+  @IsEmail({}, { message: 'incorrect email structure' })
   email: string;
+
   username: string;
   password: string;
   token: string;
